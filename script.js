8e25bedb96c2a0c473c7cfe81b5101bd259f9afb
Yuliia Shmagina <fitnessyulya@gmail.com>
2017-08-28 01:14:09 +0300

    js function

---
+++ script.js
@@ -0,0 +1,13 @@
+// var myInputs = {};
+// Array.prototype.forEach.call(document.querySelectorAll('input'), function (ths){
+//     myInputs[ths.getAttribute('id')] = ths;
+// })
+//
+// console.log(myInputs);
+//
+
+function HelloWorld() {
+    console.log( 'Hello World!' )
+}
+
+HelloWorld ();
