cddbba930b0aaa717f1a6586fbbcaa70d6dbbd01
Yuliia Shmagina <fitnessyulya@gmail.com>
2017-08-29 06:58:06 +0300

    script button

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// var myInputs = {};
-// Array.prototype.forEach.call(document.querySelectorAll('input'), function (ths){
-//     myInputs[ths.getAttribute('id')] = ths;
-// })
+// function HelloWorld() {
+//     console.log( 'Hello World!' )
+// }
 //
-// console.log(myInputs);
+// HelloWorld ();
+
+// // 1
+console.log(document.querySelector('#input_id').value);
 //
-
-function HelloWorld() {
-    console.log( 'Hello World!' )
-}
-
-HelloWorld ();
+//
+// // 2
+// function daivalue (id) {
+//     var value;
+//     value = document.getElementById(id).value;
+//     document.getElementById('#input_id').value = value;
+// }
+//
+// daivalue (id);
