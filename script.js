0e484847aa86347dd7e67d82adb4ce85c40542ce
Yuliia Shmagina <fitnessyulya@gmail.com>
2017-09-05 02:20:54 +0300

    first array

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ function getGithubData() {
         })
         .then(function(data) {
         console.log(data);
+
+        var numberOne = data[0];
+        console.log(numberOne);
     })
 }
 
 searchButton.addEventListener('click', getGithubData);
 
+var person = {
+    name: ['Bob', 'Smith'],
+    age: 32,
+    gender: 'male',
+    interests: ['music', 'skiing'],
+};
+
+var arr1 = [1, 2, 3];
+var numberTwo = arr1[1];
+console.log(numberTwo); //3
